61706288b2f53c029c82c5ac850135a1f51d25d8
rama.fall <ramafall49@gmail.com>
2023-11-20 08:58:30 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/kiliwebsite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kiliwebsite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kili Building',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Kili Building'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text('Toutes les fonctionnalites disponibles')),
+      body: Container(
+        color: Colors.white,
+        child: Image.asset(
+          'images/img1.png',
+        ),
+      ),
+    ));
+  }
+}
