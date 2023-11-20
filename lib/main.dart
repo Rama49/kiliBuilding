86241c3d60c925459d8c5483a2ac673952d625e1
YacineDiop <yacinediopkebe@gmail.com>
2023-11-20 18:07:04 +0000

    fonctionnalité

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kiliwebsite/Component/SectionFonctionnalite/Fonctionnalite.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,39 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Kili Building',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Kili Building'),
+      home: const Fonctionnalite(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-          centerTitle: true,
-          title: Text('Toutes les fonctionnalites disponibles')),
-      body: Container(
-        color: Colors.white,
-        child: Image.asset(
-          'images/img1.png',
-        ),
-      ),
-    ));
   }
 }
