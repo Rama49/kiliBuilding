cc66c36407568ea393ff04b21990c83fb6537f07
rama.fall <ramafall49@gmail.com>
2023-11-21 10:56:50 +0000

    regrouper le travail

--- lib/Component/sectionEntrepreneurs/Entrepreneur.dart
+++ lib/Component/sectionEntrepreneurs/Entrepreneur.dart
@@ -7,53 +7,40 @@ class Entrepreneur extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-          margin: EdgeInsets.only(top: 100, bottom: 100),
-          child: ListView(children: [
-            TitleReu(
-                titre:
-                    "Des entrepreneurs et des particulier nous font deja confiance",
-                soustexte: "Faire un site web n'a jamais été aussi simple"),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "clockcomputer.png",
-                    width: 300,
-                  ),
-                  imageText(
-                      image: Image.asset('ngirane.png', width: 50),
-                      titre: "NGUIRANE At Tidiani ",
-                      sousTitre: "Fondateur de Kalam",
-                      texte:
-                          "\nLorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut sed\n hac. Vel donec mauris turpis venenatis sed faucibus. Lorem ipsum dolor sit amet\n consectetur. Mauris etiam id volutpat ut sed hac. Vel donec mauris turpis venenatis \nsed faucibus.Lorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut \nsed hac. Vel donec mauris turpis venenatis sed faucibus")
-                ],
-              ),
+    return Column(
+      children: [
+        TitleReu(
+            titre:
+                "Des entrepreneurs et des particulier nous font deja confiance",
+            soustexte: "Faire un site web n'a jamais été aussi simple"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "clockcomputer.png",
+              width: 300,
             ),
-          
-          //second row
-          
-            Column(
-              children: [
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      imageText(
-                          image: Image.asset('ngirane.png', width: 50),
-                          titre: "NGUIRANE At Tidiani ",
-                          sousTitre: "Fondateur de Kalam",
-                          texte:
-                              "\nLorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut sed\n hac. Vel donec mauris turpis venenatis sed faucibus. Lorem ipsum dolor sit amet\n consectetur. Mauris etiam id volutpat ut sed hac. Vel donec mauris turpis venenatis \nsed faucibus.Lorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut \nsed hac. Vel donec mauris turpis venenatis sed faucibus"),
-                      Image.asset("imageleft.png", width: 300),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ]),
-        );
-
+            imageText(
+                image: Image.asset('ngirane.png', width: 50),
+                titre: "NGUIRANE At Tidiani ",
+                sousTitre: "Fondateur de Kalam",
+                texte:
+                    "\nLorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut sed\n hac. Vel donec mauris turpis venenatis sed faucibus. Lorem ipsum dolor sit amet\n consectetur. Mauris etiam id volutpat ut sed hac. Vel donec mauris turpis venenatis \nsed faucibus.Lorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut \nsed hac. Vel donec mauris turpis venenatis sed faucibus")
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            imageText(
+                image: Image.asset('ngirane.png', width: 50),
+                titre: "NGUIRANE At Tidiani ",
+                sousTitre: "Fondateur de Kalam",
+                texte:
+                    "\nLorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut sed\n hac. Vel donec mauris turpis venenatis sed faucibus. Lorem ipsum dolor sit amet\n consectetur. Mauris etiam id volutpat ut sed hac. Vel donec mauris turpis venenatis \nsed faucibus.Lorem ipsum dolor sit amet consectetur. Mauris etiam id volutpat ut \nsed hac. Vel donec mauris turpis venenatis sed faucibus"),
+            Image.asset("imageleft.png", width: 300),
+          ],
+        ),
+      ],
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kiliwebsite/pages/test.dart';
+import 'package:kiliwebsite/Component/sectionEntrepreneurs/Entrepreneur.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,7 +10,8 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Test()          ],
+            Entrepreneur(),
+          ],
         ),
       ),
     );
