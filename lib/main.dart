b67c5532f2d509a5c99b267a87541d30805057a3
MohamedLoum1001 <mloum137@gmail.com>
2023-11-23 11:32:20 +0000

    configuration firebase flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kiliwebsite/pages/home.dart';
 
-
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // ignore: prefer_const_constructors
+  await Firebase.initializeApp(
+      // ignore: prefer_const_constructors
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDpJPbimIX1LdCMLl92Dm5MmLOY61wDBd8",
+          appId: "1:74389324499:web:e79446474762d087704a26",
+          messagingSenderId: "74389324499",
+          projectId: "kili-app-builder"));
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
