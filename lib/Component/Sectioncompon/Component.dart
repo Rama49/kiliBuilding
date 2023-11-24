818bfeaa11f1818a08edbf30aff2018b4a059e87
ndeyenini1998 <sendeyenini@gmail.com>
2023-11-24 14:26:00 +0000

    ajouter

--- lib/Component/Sectioncompon/Component.dart
+++ lib/Component/Sectioncompon/Component.dart
@@ -9,65 +9,57 @@ class Component extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-     
-    Padding(
+    return Padding(
       padding: EdgeInsets.only(top: 90),
       child: Column(
         children: [
-           SizedBox(height: 20),
-          TitleReu(titre: "Composants", soustexte: '',),
+          SizedBox(height: 20),
+          TitleReu(titre: "Composants", soustexte: ''),
           buildRow(
             context,
             [
               CardNavigationData('Button', '5 Components', Icons.back_hand, Colors.red, Security()),
-              CardNavigationData('Input', '2 Components', Icons.input_outlined, Colors.red,Security()),
+              CardNavigationData('Input', '2 Components', Icons.input_outlined, Colors.red, Security()),
               CardNavigationData('Card', '4 Components', Icons.card_giftcard, Colors.red, Security()),
-              CardNavigationData('Header', '4 Components', Icons.headset_rounded, Colors.red,Security()),
+              CardNavigationData('Header', '4 Components', Icons.headset_rounded, Colors.red, Security()),
             ],
-            
           ),
           // Les autres rangées vont ici
-           buildRow(
+          buildRow(
             context,
             [
               CardNavigationData('Grids', '5 Components', Icons.grid_goldenratio, Colors.red, Security()),
               CardNavigationData('Selects', '2 Components', Icons.select_all, Colors.red, Security()),
               CardNavigationData('Route', '4 Components', Icons.route_outlined, Colors.red, Security()),
-              CardNavigationData('Border', '4 Components', Icons.border_bottom_outlined, Colors.red,Security()),
+              CardNavigationData('Border', '4 Components', Icons.border_bottom_outlined, Colors.red, Security()),
             ],
-            
           ),
-           buildRow(
+          buildRow(
             context,
             [
               CardNavigationData('Pagination', '5 Components', Icons.pages_outlined, Colors.red, Security()),
               CardNavigationData('Sidebar', '2 Components', Icons.view_sidebar, Colors.red, Security()),
               CardNavigationData('Toggles', '4 Components', Icons.toggle_off_outlined, Colors.red, Security()),
-              CardNavigationData('Navigation', '4 Components', Icons.navigate_before_outlined, Colors.red,Security()),
+              CardNavigationData('Navigation', '4 Components', Icons.navigate_before_outlined, Colors.red, Security()),
             ],
-            
           ),
-           buildRow(
+          buildRow(
             context,
             [
               CardNavigationData('Button', '5 Components', Icons.bolt_outlined, Colors.red, Security()),
               CardNavigationData('Input', '2 Components', Icons.input_rounded, Colors.red, Security()),
               CardNavigationData('Text', '4 Components', Icons.text_format_outlined, Colors.red, Security()),
-              CardNavigationData('Table', '4 Components', Icons.tab_outlined, Colors.red,Security()),
+              CardNavigationData('Table', '4 Components', Icons.tab_outlined, Colors.red, Security()),
             ],
-            
-          )
+          ),
         ],
-        
       ),
     );
-    
   }
 
   Widget buildRow(BuildContext context, List<CardNavigationData> cardDataList) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.center, // Centrez horizontalement
       children: cardDataList.map((cardData) {
         return buildCard(context, cardData);
       }).toList(),
@@ -82,16 +74,13 @@ class Component extends StatelessWidget {
           MaterialPageRoute(builder: (context) => cardData.route),
         );
       },
-      child: Padding(
-        padding: EdgeInsets.all(25),
-        child: Card(
-          color: Colors.white,
-          child: Cartsimple(
-            title: cardData.title,
-            subTitle: cardData.subTitle,
-            icon: cardData.icon,
-            iconColor: cardData.iconColor,
-          ),
+      child: Card(
+        color: Colors.white,
+        child: Cartsimple(
+          title: cardData.title,
+          subTitle: cardData.subTitle,
+          icon: cardData.icon,
+          iconColor: cardData.iconColor,
         ),
       ),
       splashColor: Colors.blue,
@@ -109,5 +98,3 @@ class CardNavigationData {
 
   CardNavigationData(this.title, this.subTitle, this.icon, this.iconColor, this.route);
 }
-
-
