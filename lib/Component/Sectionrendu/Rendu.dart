99c799b0ef0e3f3d59daf411c9d7d2548d1ae93b
ndeyenini1998 <sendeyenini@gmail.com>
2023-11-28 13:57:09 +0000

    changer de branche

---
+++ lib/Component/Sectionrendu/Rendu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Rendu extends StatelessWidget {
+  const Rendu({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Titre du Rendu',
+          style: TextStyle(
+            fontSize: 18, // Changer la taille du texte du titre si nécessaire
+            fontWeight: FontWeight.bold, // Changer le style du texte du titre si nécessaire
+          ),
+        ),
+        Container(
+          color: Colors.white,
+          child: Text('fgdgdhfhfhhfh'),
+        ),
+      ],
+    );
+  }
+}
